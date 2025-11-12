04b3f9bf74254c01fe4c4fab65a77a7ac0dd14cb
Agent1c <simon.ndlovu1sa@gmail.com>
2025-11-12 21:49:26 +0200

    Update and upload App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,28 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import UserProfile from './components/UserProfile'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import Counter from './components/Counter'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <h2>Hello rasta</h2>
       <WelcomeMessage />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
+      <Counter />
       <Footer />
     </>
-  )
+  );
 }
 
 export default App
