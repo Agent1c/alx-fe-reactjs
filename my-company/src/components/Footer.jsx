471ee0c10362458915dfac35fa15edde3209a9d7
Agent1c <simon.ndlovu1sa@gmail.com>
2025-11-16 23:09:47 +0200

    add feature: footer

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            <p>Author: Hege Refsnes<br /><a href="mailto:hege@example.com">agent1c@example.com</a></p>
+        </div>
+    )
+}
+
+export default Footer;
